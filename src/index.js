b2936171895380e45fa9ab28feac82cd7a428689
yangyxu <945758912@qq.com>
2020-02-02 17:45:48 +0800

    update deps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require('znui-react');
-module.exports = {
-    'Input': require('./Input.js'),
-    'Inputs': require('./Inputs.js'),
-    'Textarea': require('./Textarea.js')
-};
+module.exports = znui.react.registerComponent('input', {
+    components: {
+        'Input': require('./Input.js'),
+        'Inputs': require('./Inputs.js'),
+        'Textarea': require('./Textarea.js')
+    }
+}).components();
